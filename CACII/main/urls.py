f5c721be7968925b7ec38e17380416a30a2f7417
Andrew Oh <54751777+yandrewyo@users.noreply.github.com>
2024-06-30 16:29:19 -0700

    create first version of navbar

--- CACII/main/urls.py
+++ CACII/main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("/timeline", views.timeline, name="timeline"),
+    path("/about", views.about, name="about"),
+    path("/profile", views.profile, name="profile"),
 ]
--- CACII/main/views.py
+++ CACII/main/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def timeline(request):
+    return render(request, "timeline.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
